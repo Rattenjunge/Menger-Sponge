a6f9e20eadf0c50fba5f1a3e04f358cd2ec3d298
Rattenjunge <les.dams96@gmail.com>
2021-08-11 19:21:02 +0200

    [Update] Project starts now with 3 iterations

---
+++ Convex Hull/Assets/Scripts/DrawConvexHull.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawConvexHull : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private PointSpawner pointSpawner;
+    [SerializeField] List<GameObject> ConvexHull = new List<GameObject>();
+    [SerializeField] private LineRenderer lineRend;
+
+
+   private void Update()
+   {
+       if (pointSpawner.pointList.Count > 0)
+       {
+           ConvexHull = JarvisMarchAlgorithm.GetConvexHull(pointSpawner.pointList);
+           if (ConvexHull.Count > 0)
+           {
+  
+               lineRend.positionCount = ConvexHull.Count;
+               for (int i = 0; i < ConvexHull.Count; i++)
+               {
+                   lineRend.SetPosition(i, ConvexHull[i].transform.position);
+               }
+           }
+       }
+   }
+  
+
+  // void Test()
+  // {
+  //
+  //     if (pointSpawner.pointList.Count > 0)
+  //     {
+  //         ConvexHull = JarvisMarchAlgorithm.GetConvexHull(pointSpawner.pointList);
+  //         if (ConvexHull.Count > 0)
+  //         {
+  //
+  //             lineRend.positionCount = ConvexHull.Count -1;
+  //             for (int i = 0; i < ConvexHull.Count - 1; i++)
+  //             {
+  //                 lineRend.SetPosition(i, ConvexHull[i].transform.position);
+  //             }
+  //         }
+  //     }
+  // }
+  //
+  //
+}
